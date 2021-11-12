80c1d85e60b987a72ed65b433074c08b8af9bbe2
Arun3sh <m.aruneshwar@gmail.com>
2021-11-12 08:13:38 +0530

    Card style

--- src/ShowMovie.js
+++ src/ShowMovie.js
@@ -5,26 +5,35 @@ import ExpandMore from '@mui/icons-material/ExpandMore';
 import ExpandLess from '@mui/icons-material/ExpandLess';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { Button, CardActions } from '@mui/material';
+import InfoIcon from '@mui/icons-material/Info';
+import { useHistory } from 'react-router';
 
 export function ShowMovie({ name, poster, summary, rating, index }) {
 	const [show, setShow] = useState(true);
 	const styles = { display: show ? 'block' : 'none' };
+	const history = useHistory();
 	return (
-		<div className="movie" key={index}>
+		<div className="movie">
 			{/* new way */}
 			<Card sx={{ maxWidth: 300 }} className="card">
 				{/* contents */}
-
-				<CardMedia component="img" height="140" image={poster} alt={name} className="image" />
+				<img src={poster} alt={name} className="image" />
+				{/* <CardMedia component="img" height="140" image={poster} alt={name} className="image" /> */}
 				<CardContent>
 					<Typography gutterBottom variant="h5" component="div" className="heading">
 						<h4>
 							{name}
-							<IconButton aria-label="fingerprint" color="primary" onClick={() => setShow(!show)}>
+							<IconButton aria-label="Expand" color="primary" onClick={() => setShow(!show)}>
 								{!show ? <ExpandMore /> : <ExpandLess />}
+							</IconButton>
+							<IconButton
+								aria-label="To Trailer"
+								color="primary"
+								onClick={() => history.push(`/movies/${index}`)}
+							>
+								<InfoIcon />
 							</IconButton>
 						</h4>
 						<h2 className="rating">⭐{rating}</h2>
