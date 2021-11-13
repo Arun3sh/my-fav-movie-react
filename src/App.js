6a1fc6519d557a8b7743244900e54370bc51ba39
Arun3sh <m.aruneshwar@gmail.com>
2021-11-13 13:44:27 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import { useState } from 'react';
 import './App.css';
-import { ShowMovie } from './ShowMovie';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { InnitialMovie } from './InnitialMovie';
 import { Addmovie } from './AddMovie';
+import { Editmovie } from './Editmovie';
 import { Nav } from './Routerlink';
 import { Error } from './ErrorPage';
 import { MovieDetail } from './MovieDetail';
+import { Movielsit } from './Movielist';
 
 function App() {
 	const MyFavMovie = InnitialMovie();
-	const [movies] = useState(MyFavMovie);
+	const [movies, setMovies] = useState(MyFavMovie);
 
-	//setMovies([...movies, addedmovies]);
-	// };
 	return (
 		<div className="App">
 			{/* Navbar component */}
@@ -31,26 +30,16 @@ function App() {
 				</Route>
 
 				<Route exact path="/movies">
-					<section className="fav-movies">
-						{movies.map(({ name, poster, summary, rating }, index) => (
-							<ShowMovie
-								name={name}
-								poster={poster}
-								summary={summary}
-								rating={rating}
-								index={index}
-								key={index}
-							/>
-						))}
-					</section>
+					<Movielsit movies={movies} setMovies={setMovies} />
 				</Route>
 
 				<Route path="/movies/add">
-					<Addmovie />
+					<Addmovie movies={movies} setMovies={setMovies} />
 				</Route>
 
-				<Route path="/movies/edit">"Edit movies here"</Route>
-				<Route path="/movies/delete">"Delete movies here"</Route>
+				<Route path="/movies/edit/:id">
+					<Editmovie movies={movies} setMovies={setMovies} />
+				</Route>
 
 				<Route exact path="/movies/:id">
 					<MovieDetail movies={movies} />
