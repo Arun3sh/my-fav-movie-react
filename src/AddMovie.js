d06a114a830c41a9e8c71e2f86b6899f0f69a98a
Arun3sh <m.aruneshwar@gmail.com>
2021-11-13 13:41:45 +0530

    To add movie

--- src/AddMovie.js
+++ src/AddMovie.js
@@ -4,28 +4,21 @@ import './App.css';
 import { TextField } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import DeleteIcon from '@mui/icons-material/Delete';
-import { InnitialMovie } from './InnitialMovie';
 import { useHistory } from 'react-router';
-// import { ShowMovie } from './ShowMovie';
 
-export function Addmovie() {
-	const MyFavMovie = InnitialMovie();
-	const [movies, setMovies] = useState(MyFavMovie);
-
+export function Addmovie({ movies, setMovies }) {
 	const [name, setName] = useState('');
 	const [poster, setPoster] = useState('');
 	const [summary, setSummary] = useState('');
 	const [rating, setRating] = useState('');
+	const [trailer, setTrailer] = useState('');
 	const history = useHistory();
 
 	const addMovie = () => {
 		const newMovie = { name, poster, summary, rating };
-		// setMovies([...movies, newMovie]);
-		const addedmovies = [...movies, newMovie];
-		setMovies(addedmovies);
-		console.log(addedmovies);
-		console.log(movies);
 
+		setMovies([...movies, newMovie]);
+		clearEntry();
 		history.push('/movies');
 	};
 
@@ -34,10 +27,11 @@ export function Addmovie() {
 		setPoster('');
 		setSummary('');
 		setRating('');
+		setTrailer('');
 	};
 	return (
 		<div>
-			<h1>My Favorite Movies</h1>
+			<h1>Add Movie</h1>
 			<div className="addMovie">
 				<form className="myForm" noValidate autoComplete="off">
 					<TextField
@@ -78,6 +72,15 @@ export function Addmovie() {
 						onChange={(e) => setRating(e.target.value)}
 					/>
 
+					<TextField
+						id="outlined-basic"
+						label="Movie Trailer"
+						variant="outlined"
+						type="text"
+						value={trailer}
+						onChange={(e) => setTrailer(e.target.value)}
+					/>
+
 					<div className="add-cancel">
 						<Button
 							variant="outlined"
@@ -91,12 +94,12 @@ export function Addmovie() {
 						<Button
 							variant="outlined"
 							type="button"
-							className="cancelbtn"
+							className="resetBtn"
 							color="error"
 							onClick={clearEntry}
 							startIcon={<DeleteIcon />}
 						>
-							Cancel
+							Reset
 						</Button>
 					</div>
 				</form>
