1c1b27fe3806b33786c276b3e8bc348cf7cbb28f
Arun3sh <m.aruneshwar@gmail.com>
2021-11-13 13:43:16 +0530

    added condition to help route

--- src/MovieDetail.js
+++ src/MovieDetail.js
@@ -1,19 +1,39 @@
-import { useParams } from 'react-router';
+import { useHistory, useParams } from 'react-router';
+import { Button } from '@mui/material';
+import KeyboardBackspaceRoundedIcon from '@mui/icons-material/KeyboardBackspaceRounded';
+import { Error } from './ErrorPage';
 
 export function MovieDetail({ movies }) {
 	const { id } = useParams();
-	const movie = movies[id];
+	const history = useHistory();
+	console.log(id.match(/^[0-9]+$/) != null);
 
-	return (
-		<div className="movie-detail">
-			<div className="trailer">
-				<iframe width="100%" height="450" src={movie.trailer} title={movie.name}></iframe>
+	const movie = movies[id];
+	if ((movies.length > id) & (id.match(/^[0-9]+$/) != null)) {
+		return (
+			<div className="movie-detail">
+				<div className="trailer">
+					<iframe width="100%" height="450" src={movie.trailer} title={movie.name}></iframe>
+				</div>
+				<div className="heading-summary">
+					<div className="heading">
+						<h2>{movie.name}</h2>
+						<h2 className="rating">⭐{movie.rating}</h2>
+					</div>
+					<div className="summary">{movie.summary}</div>
+				</div>
+				<div className="nav-button">
+					<Button
+						variant="outlined"
+						onClick={() => history.goBack()}
+						startIcon={<KeyboardBackspaceRoundedIcon />}
+					>
+						Back
+					</Button>
+				</div>
 			</div>
-			<div className="heading">
-				<h2>{movie.name}</h2>
-				<h2 className="rating">⭐{movie.rating}</h2>
-			</div>
-			<div className="summary">{movie.summary}</div>
-		</div>
-	);
+		);
+	} else {
+		return <Error />;
+	}
 }
