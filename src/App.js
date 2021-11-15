0b510a5587e62988f1857d391b96ebfda98b0ccf
Arun3sh <m.aruneshwar@gmail.com>
2021-11-15 19:28:59 +0530

    context added

--- src/App.js
+++ src/App.js
@@ -1,55 +1,63 @@
-import { useState } from 'react';
+import { createContext, useState } from 'react';
 import './App.css';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { InnitialMovie } from './InnitialMovie';
 import { Addmovie } from './AddMovie';
 import { Editmovie } from './Editmovie';
-import { Nav } from './Routerlink';
+import { Nav } from './Navbar';
 import { Error } from './ErrorPage';
 import { MovieDetail } from './MovieDetail';
 import { Movielsit } from './Movielist';
+
+export const themeContext = createContext('null');
 
 function App() {
 	const MyFavMovie = InnitialMovie();
 	const [movies, setMovies] = useState(MyFavMovie);
 
+	const [theme, setTheme] = useState('light');
+	const style = {
+		backgroundColor: theme === 'dark' ? 'black' : 'white',
+	};
 	return (
-		<div className="App">
-			{/* Navbar component */}
-			<Nav />
+		<themeContext.Provider value={[theme, setTheme]}>
+			<div className="App" style={style}>
+				{/* Navbar component */}
+				<Nav />
 
-			<Switch>
-				<Route exact path="/">
-					Home
-				</Route>
+				<Switch>
+					<Route exact path="/">
+						Home
+					</Route>
 
-				<Route path="/about">This is where you can find my fav movies!</Route>
+					<Route path="/about">This is where you can find my fav movies!</Route>
 
-				<Route path="/films">
-					<Redirect to="/movies" />
-				</Route>
+					<Route path="/films">
+						<Redirect to="/movies" />
+					</Route>
 
-				<Route exact path="/movies">
-					<Movielsit movies={movies} setMovies={setMovies} />
-				</Route>
+					<Route exact path="/movies">
+						<Movielsit movies={movies} setMovies={setMovies} />
+					</Route>
 
-				<Route path="/movies/add">
-					<Addmovie movies={movies} setMovies={setMovies} />
-				</Route>
+					<Route path="/movies/add">
+						<Addmovie movies={movies} setMovies={setMovies} />
+					</Route>
 
-				<Route path="/movies/edit/:id">
-					<Editmovie movies={movies} setMovies={setMovies} />
-				</Route>
+					<Route path="/movies/edit/:id">
+						<Editmovie movies={movies} setMovies={setMovies} />
+					</Route>
 
-				<Route exact path="/movies/:id">
-					<MovieDetail movies={movies} />
-				</Route>
+					<Route exact path="/movies/:id">
+						<MovieDetail movies={movies} />
+					</Route>
 
-				<Route path="**">
-					<Error />
-				</Route>
-			</Switch>
-		</div>
+					<Route path="**">
+						<Error />
+					</Route>
+				</Switch>
+			</div>
+		</themeContext.Provider>
 	);
 }
 
